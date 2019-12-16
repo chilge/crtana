b97de7011ae25bb0d8c76fa15f345bb0fcac75a4
Chris Hilgenberg <Chris.Hilgenberg@colostate.edu>
2019-12-16 10:02:05 -0600

    updated for use with north wall data

--- src/Analyzer.C
+++ src/Analyzer.C
@@ -43,26 +43,42 @@ bool isFloat(string s){
 
 }
 
+string formatMac(int mac){
+	string out;
+	if(mac<10) out="00";
+	else if(mac<100) out="0";
+	out+=to_string(mac);	
+
+	return out;
+}
+
 //main code body
 int main() {
 
-	string in;
+	string in, file1, file2;
 
-	cout << "." << endl;
-	cin >> in;
+	cout << "Welcome to the analyzer module!" << '\n'
+	     << "You will be asked to enter the full path" << '\n'
+	     << " to each of a pair of root ntuples, one"  << '\n'
+	     << " for each CRT layer..." <<  endl;
+	cout << "Enter the first file path." << endl;
+	cin >> file1;
+	cout << "Enter the second file path." << endl;
+	cin >> file2;
+	cout << "Attempting to analyze..." << endl;
 
 	gROOT->Reset();
 
-	mppc m();
-	if(m.fTreeHod==0||m.fTreeSelf==0) {
-		cout << "ERROR: failed to initialize trees. Exiting..." << endl;
-		return 0;
-	}
-
-	TString fname = "Trees/"+m.ormDirName+".root"; //output file path
+	mppc m(file1,file2);
+	string fname = file1;
+	while(fname.find("/")!=UINT64_MAX)
+        	fname=fname.substr(fname.find("/")+1,fname.size());
+	fname=fname.substr(0,fname.find("inner"));
+	fname=fname.substr(0,fname.find("outer"));
+	fname="./Trees/"+fname+".root";
 
 	//check if file already exists, exit if it does
-	if(!gSystem->AccessPathName(fname)) {
+	if(!gSystem->AccessPathName(fname.c_str())) {
 		cout << "ERROR: these data appear to already have been processed..." << '\n' 
 			<< "    output tree found: " << fname << '\n'
 			<< "    Exiting..." << endl;
@@ -76,19 +92,15 @@ int main() {
 
 	Double_t gain[32], gainerr[32], gped[32], gpederr[32], gchisqr[32], gndf[32];
 	Double_t pmean[32], pmeanerr[32], psig[32], psigerr[32], pconst[32], pconsterr[32];
-	Int_t lynfit[32];
-	Double_t lypeak[32], lypeakerr[32], lyfwhm[32], lynorm[32], lynormerr[32]; 
-	Double_t lyw[32], lywerr[32], lympv[32], lympverr[32], lygsig[32], lygsigerr[32];
 	Bool_t chconfig[32];
 	int mac;
 
 	//output file 
 	cout << "Opened output file '" << fname << "'" << endl;
-	TFile f(fname,"RECREATE");
+	TFile f(fname.c_str(),"RECREATE");
 	TTree* tree = new TTree("orm","analysis results from raw ORM test data");
 	
 	//run info
-	//tree->Branch("run",              &run,     "run/I");
 	tree->Branch("mac5",             &mac,     "mac/I");
 	tree->Branch("chConfig",         chconfig, "chConfig[32]/b");
 
@@ -108,20 +120,6 @@ int main() {
 	tree->Branch("pedConst",         pconst,   "pedConst[32]/D");
 	tree->Branch("pedConstErr",      pconsterr,"pedConstErr[32]/D");
 
-	//LY fit info
-	tree->Branch("lyNFit",           lynfit,   "lyNFit[32]/I");
-	tree->Branch("lyPeak",           lypeak,   "lyPeak[32]/D");
-	tree->Branch("lyPeakErr",        lypeakerr,"lyPeakErr[32]/D");
-	tree->Branch("lyFWHM",           lyfwhm,   "lyFWHM[32]/D");
-	tree->Branch("lyLandauWidth",    lyw,      "lyLandauWidth[32]/D");
-	tree->Branch("lyLandauWidthErr", lywerr,   "lyLandauWidthErr[32]/D");
-	tree->Branch("lyLandauMPV",      lympv,    "lyLandauMPV[32]/D");
-	tree->Branch("lyLandauMPVErr",   lympverr, "lyLandauMPVErr[32]/D");
-	tree->Branch("lyNorm",           lynorm,   "lyNorm[32]/D");
-	tree->Branch("lyNormErr",        lynormerr,"lyNormErr[32]/D");
-	tree->Branch("lyGaussSigma",     lygsig,   "lyGaussSigma[32]/D");
-	tree->Branch("lyGaussSigmaErr",  lygsigerr,"lyGaussSigmaErr[32]/D");
-
 	for(auto feb=febchans.begin(); feb!=febchans.end(); feb++) 
 	{
 		mac = (*feb).first;
@@ -134,19 +132,20 @@ int main() {
 		cout << "processing data for mac5 " << mac << endl;
 		cout << " calibrating..." << endl;
 		m.Cal(mac); //pedestal then gain calibration
-		cout << " LY fit..." << endl;
-		m.PlotSingleChanCutLoop('h',mac,2.5); //LY fits, single chan cut @ thr=2.5 PE
 
 		//filling local variables from cal/ana data files
 		cout << " extracting analysis data..." << endl;
 		m.FillPedArr(mac,pmean,pmeanerr,psig,psigerr,pconst,pconsterr);
 		m.FillGainArr(mac,gain,gainerr,gped,gpederr,gchisqr,gndf);
-		m.FillLYArr(mac,lynfit,lypeak,lyfwhm,lyw,lywerr,lympv,lympverr,lynorm,lynormerr,lygsig,lygsigerr);
-		for(size_t ch=0; ch<32; ch++) {
-			if(lynfit[ch]==0)
-				continue;
-			lypeakerr[ch] = (lyfwhm[ch]/2.355)/sqrt(lynfit[ch]);
+
+		cout << " generating charge spectra..." << endl;
+		TList *list = new TList();
+		for(int ch=0; ch<32; ch++){
+			if(chconfig[ch])
+				list->Add(m.PlotSpectrum(mac,ch,0,0));
 		}
+		string listname="spectra"+formatMac(mac);
+		list->Write(listname.c_str(),TObject::kSingleKey);
 
 		cout << " filling tree..." << endl;
 		tree->Fill();
